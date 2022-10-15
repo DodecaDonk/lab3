b22e3af3631087bc1c0b2219cdf2e35221797b9a
DodecaDonk <danielyoon2003@gmail.com>
2022-10-14 19:39:08 -0700

    LabReport3

--- ListExamples.java
+++ ListExamples.java
@@ -3,6 +3,12 @@ import java.util.List;
 
 interface StringChecker { boolean checkString(String s); }
 
+class StringImp implements StringChecker{
+  public boolean checkString(String s){
+    return true;
+  }
+}
+
 class ListExamples {
 
   // Returns a new list that has all the elements of the input list for which
--- ListTests.java
+++ ListTests.java
@@ -1,6 +1,19 @@
 import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+
 import org.junit.*;
 
 public class ListTests {
-    
+    @Test 
+	public void filterTest() {
+    int[] input1 = { 3 };
+    ArrayList<String> input = new ArrayList<String>();
+    input.add("Hello");
+    input.add("my");
+    input.add("name");
+    input.add("Daniel");
+    StringChecker sc = new StringImp();
+    assertEquals(input, ListExamples.filter(input, sc));
+	}
 }
